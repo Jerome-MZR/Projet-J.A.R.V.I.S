468418e9987d77f480fc58557dac4d18c756ddae
serumDev <jerome.mazouiller@gmail.com>
2018-08-01 15:42:36 +0200

    Add files via upload

---
+++ Projet J.A.R.V.I.S.py
@@ -0,0 +1,23 @@
+#J.A.R.V.I.S. (Just A Rather Very Intelligent System)
+
+INFO = '''
+        +---------------------------------------+
+        |.....JARVISE VIRTUAL INTELLIGENCE......|
+        +---------------------------------------+
+        |#Author:  Jerome MAZOUILLER            |
+        |#Date:    03/05/2018                   |
+        | Rev:     0.1                          |
+        +---------------------------------------+
+       '''
+
+# Import OS
+import os
+# Import Math: module qui permet d’avoir accès aux fonctions mathématiques
+import math
+#Import random: module pour génération des nombres aléatoires.
+import random
+
+
+
+# On met le programme en pause pour éviter qu'il ne se referme (Windows)
+os.system("pause")
